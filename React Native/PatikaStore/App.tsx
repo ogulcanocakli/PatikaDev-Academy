bd60fdc38103768c9e3291f1228278e8c0937c06
Oğulcan Ocaklı <ogulcan.ocakli@gmail.com>
2023-04-18 15:51:42 +0300

    Added search feature

--- React Native/PatikaStore/App.tsx
+++ React Native/PatikaStore/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView, ScrollView, Text, TextInput, FlatList, StyleSheet } from "react-native";
 
 import ProductCard from "./src/components/Card/ProductCard";
@@ -6,17 +6,29 @@ import product_data from "./src/products.json";
 
 function App() {
 
+  const [dataList, setDataList] = useState(product_data);
+  const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    filteredData();
+  }, [search])
+
+  const filteredData = () => {
+    const filteredData = product_data.filter(q => q.title.toLocaleLowerCase().trim().includes(search.toLocaleLowerCase().trim()))
+    setDataList([...filteredData])
+  }
+
   const renderProduct = ({ item }: any) => <ProductCard product={item} />;
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView>
         <Text style={styles.title}>PatikaStore</Text>
-        <TextInput style={styles.input} placeholder="Ara" />
+        <TextInput style={styles.input} placeholder="Ara"  onChange={filteredData} onChangeText={(e) => setSearch(e)}/>
 
         <FlatList
           keyExtractor={(item, index) => item.id.toString()}
-          data={product_data}
+          data={dataList}
           renderItem={renderProduct}
           numColumns={2}
         />
